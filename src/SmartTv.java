d19529356ac61d0b33e49d386479f8c4a8957fea
MarcosWinther <winthermarcos@gmail.com>
2024-06-18 12:21:29 -0300

    "feat: novas implementações nas classes 'SmartTv.java' e 'Usuario.java'"

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -2,4 +2,45 @@ public class SmartTv {
    boolean ligada = false;
    int canal = 1;
    int volume = 25;
+
+   public void ligar() {
+      ligada = true;
+   }
+
+   public void desligar() {
+      ligada = false;
+   }
+
+   public void aumentarVolume() {
+      if(volume >= 0)
+         System.out.println("A TV já está sem som.");
+      else {
+         volume++;
+         System.out.println("Aumentando volume para: " + volume);
+      }
+   }
+
+   public void diminuirVolume() {
+      if(volume >= 0)
+         System.out.println("A TV já está sem som.");
+      else {
+         volume--;
+         System.out.println("Diminuindo o volume para: " + volume);
+      }
+   }
+
+   public void mudarCanal(int novoCanal) {
+      canal = novoCanal;
+      System.out.println("Novo canal é: " + canal);
+   }
+
+   public void aumentarCanal() {
+      canal++;
+      System.out.println("Novo canal é: " + canal);
+   }
+
+   public void diminuirCanal() {
+      canal--;
+      System.out.println("Novo canal é: " + canal);
+   }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -6,5 +6,15 @@ public class Usuario {
         System.out.println("TV ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.aumentarCanal();
+
+        smartTv.mudarCanal(30);
     }
 }
